9d00f4cc31d40c265771a34f837da9ef16e59464
Heerraval182 <d24IT182@charusat.edu.in>
2026-08-03 06:05:57 +0000

    json

---
+++ src/utils/fileHelper.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+
+function readJSON(filePath) {
+  return JSON.parse(fs.readFileSync(filePath, 'utf8'));
+}
+
+function writeJSON(filePath, data) {
+  fs.writeFileSync(filePath, JSON.stringify(data, null, 2));
+}
+
+module.exports = {
+  readJSON,
+  writeJSON,
+};
